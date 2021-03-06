b2a1a8b8f5d33df6c2a1a8f61f18801a22cfee7c
Tsuwa16 <tk.suwa16@gmail.com>
2021-03-06 15:17:12 +0900

    修正：ユーザー登録画面へバリデーションを実装

--- src/main/java/com/app/login/SignupController.java
+++ src/main/java/com/app/login/SignupController.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,7 +48,7 @@ public class SignupController {
 	//データバインド結果の受け取り
 	@PostMapping("/signup")
 	public String postSignUp(
-			@ModelAttribute SignupForm form , 
+			@ModelAttribute @Validated SignupForm form , 
 			BindingResult bindingResult ,
 			Model model) {
 		
--- src/main/java/com/app/login/domain/model/SignupForm.java
+++ src/main/java/com/app/login/domain/model/SignupForm.java
@@ -2,6 +2,14 @@ package com.app.login.domain.model;
 
 import java.util.Date;
 
+import javax.validation.constraints.AssertFalse;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import org.hibernate.validator.constraints.Length;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
@@ -9,13 +17,25 @@ import lombok.Data;
 @Data
 public class SignupForm {
 
+	@NotBlank
+	@Email
 	private String userId; //ユーザID
+	
+	@NotBlank
+	@Length(min=4 , max=100)
+	@Pattern(regexp = "^[a-zA-Z0-9]+$")
 	private String password; //パスワード
+	
+	@NotBlank
 	private String userName; //ユーザ名
 	
 	@DateTimeFormat(pattern="yyyy/MM/dd")
 	private Date birthday; //誕生日
-	
+
+	@Min(20)
+	@Max(100)
 	private int age; //年齢
+	
+	@AssertFalse
 	private boolean marriage; //結婚ステータス
 }
